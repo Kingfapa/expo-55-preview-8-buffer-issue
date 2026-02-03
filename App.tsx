f2087c1effc80cc9cb730f38c5d0d012168de056
Adam Maritz <14345584+Kingfapa@users.noreply.github.com>
2026-02-03 15:02:19 +0100

    removed qr-code dependency

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import QRCode from "react-native-qrcode-svg";
+import Svg, { Circle } from "react-native-svg";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-      <QRCode value="https://example.com" size={200} />
+      <Svg height="100" width="100">
+        <Circle
+          cx="50"
+          cy="50"
+          r="45"
+          stroke="blue"
+          strokeWidth="2.5"
+          fill="green"
+        />
+      </Svg>
     </View>
   );
 }
